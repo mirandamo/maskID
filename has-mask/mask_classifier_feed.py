623f75125fe433dc9a37b6fea981d08fd6a1d44b
Dev Ramesh <dev_ramesh@brown.edu>
2021-04-17 10:22:43 -0500

    Test

--- has-mask/mask_classifier_feed.py
+++ has-mask/mask_classifier_feed.py
@@ -9,7 +9,7 @@ import os
 
 labels_dict={False:'without mask',True:'mask'}
 color_dict={False:(0,0,255),True:(0,255,0)}
-
+#test
 def hasMask(img):
     model=load_model("./model2-010.model")
     resized=cv2.resize(img,(150,150))
